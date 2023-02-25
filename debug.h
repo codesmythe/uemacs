c8713372d00810f4d89c20afa096e85204d2f45f
Rob Gowin <robgowin@gmail.com>
2023-02-25 09:41:52 -0600

    Add debug.h

---
+++ debug.h
@@ -0,0 +1,12 @@
+//
+// Created by Robert Gowin Jr on 6/3/21.
+//
+
+#ifndef UEMACS_DEBUG_H
+#define UEMACS_DEBUG_H
+
+/* debug routines */
+void pr(const char *s);
+void rsprintf(const char *fmt, ...);
+
+#endif //UEMACS_DEBUG_H
